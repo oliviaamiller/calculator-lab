02ba198b6b2e808dccd05f2613d1c956d732a1a6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-08-26 11:42:39 -0700

    Added add UI and button listener

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // import functions
 
 // reference needed DOM elements
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
+
+addButton.addEventListener('click', () => {
+    console.log('add button clicked');
+})
 
 
 // set event listeners 

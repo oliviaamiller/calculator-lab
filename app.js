c493897bc70b5df4edd8501e39f907abd87a8ea1
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-08-26 14:17:17 -0700

    Working add feature

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions
+import { add } from './calculations.js';
+
 
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
@@ -6,9 +8,12 @@ const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
 
+
 addButton.addEventListener('click', () => {
-    console.log('add button clicked');
-})
+    const no1 = Number(addNumber1.value);
+    const no2 = Number(addNumber2.value);
+    addAnswer.textContent = add(no1, no2);
+});
 
 
 // set event listeners 

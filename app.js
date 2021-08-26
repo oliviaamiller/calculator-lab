c1699af77f94cd9dcef2f645aca8ab75baa50685
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-08-26 15:10:15 -0700

    added pythagorean theorem

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // import functions
 import { add } from './calculations.js';
-
+import { addpt } from './calculations.js';
 
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
+
+const addNumber1Pt = document.getElementById('add-pt-1');
+const addNumber2Pt = document.getElementById('add-pt-2');
+const addButtonPt = document.getElementById('add-pt-button');
+const addAnswerPt = document.getElementById('add-pt-answer');
 
 
 addButton.addEventListener('click', () => {
@@ -15,6 +20,12 @@ addButton.addEventListener('click', () => {
     addAnswer.textContent = add(no1, no2);
 });
 
+
+addButtonPt.addEventListener('click', () => {
+    const no1Pt = Number(addNumber1Pt.value);
+    const no2Pt = Number(addNumber2Pt.value);
+    addAnswerPt.textContent = addpt(no1Pt, no2Pt);
+});
 
 // set event listeners 
     // get user input(s)
